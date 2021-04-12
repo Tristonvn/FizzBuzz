5b8659908837ab363c302565200a2a9bfae6865b
Tristonvn <tristonvn@gmail.com>
2021-04-13 01:02:23 +0200

    complete project

--- FizzBuzz/FizzBuzz.cpp
+++ FizzBuzz/FizzBuzz.cpp
@@ -4,6 +4,20 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    for (int i = 1; i < 101; i++) {
+        if ((i % 5) == 0 && (i % 3) == 0) {
+            std::cout << "FizzBuzz\n";
+        }
+        else if ((i % 5) == 0) {
+            std::cout << "Buzz\n";
+        } 
+        else if ((i % 3) == 0) {
+            std::cout << "Fizz\n";
+        }
+        else {
+            std::cout << i << "\n";
+        }
+
+    }
 }
 
