5b4388d1a9cee6e5af9ddd7c7994fffd0e30955a
Tristonvn <tristonvn@gmail.com>
2021-04-13 00:56:42 +0200

    Add project files.

---
+++ FizzBuzz/FizzBuzz.cpp
@@ -0,0 +1,9 @@
+//This project will print number from 1 to 100, on multples of three fizz will be printed and on multples of five buzz will be printed
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
